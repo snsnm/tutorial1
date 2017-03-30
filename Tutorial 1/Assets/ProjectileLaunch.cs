acb1377ef0bf39946685d5e0f63252f3ae504e0a
snsnm <steve.noumbissi@yahoo.com>
2017-03-29 23:03:45 -0400

    added project1

---
+++ Tutorial 1/Assets/ProjectileLaunch.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileLaunch : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Tutorial 1/Assets/Scripts/ProjectileLauncher.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using HoloToolkit.Unity.InputModule;
+
+public class ProjectileLauncher : MonoBehaviour, IInputClickHandler {
+	public GameObject prefab;
+	// Use this for initialization
+	void Start () {
+		InputManager.Instance.PushModalInputHandler(this.gameObject);
+	}
+
+	public void OnInputClicked (InputEventData eventData){
+
+		GameObject projectile = Instantiate (prefab) as GameObject;
+		projectile.transform.position = Camera.main.transform.position + Camera.main.transform.forward * 0.85f;
+		Rigidbody rb = projectile.GetComponent<Rigidbody> ();
+		rb.velocity = Camera.main.transform.forward * 10;
+	}
+}
